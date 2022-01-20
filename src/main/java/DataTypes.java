12d7a6fb45ef1c0982f5151672831c845710260d
yardenc2003 <yardenc2003@gmail.com>
2022-01-20 10:26:34 +0200

    Added DataType enum for record data-type detection

---
+++ src/main/java/DataTypes.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+enum DataType {
+    STRING(String.class),
+    INT(Integer.class),
+    DATE(Date.class);
+
+    public Class<?> type; // the char with represents the specific terrain type
+
+    DataType(Class<?> type) {
+        this.type = type;
+    }
+}
